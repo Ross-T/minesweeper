1765e13f92684c9b6a1a7005723957ce2e3ae201
Ross-T <ross_turpin@yahoo.co.uk>
2023-11-29 11:25:18 +0000

    Added createBoard() method and Lombok dependency

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,11 +1,23 @@
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Board {
     private int size;
     private int mines;
     private char mine = '*';
     private char unknown = '-';
     private char empty = ' ';
-    private char[][] board;
+    private char[][] board = new char[size][size];
 
 
+    void createBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = empty;
+            }
+        }
+    }
 
 }
