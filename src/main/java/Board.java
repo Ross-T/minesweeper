f648cb4b3cf721ef489bb403165929cc244a29f7
Ross-T <ross_turpin@yahoo.co.uk>
2023-11-29 10:57:35 +0000

    Create Board class and add initial fields

---
+++ src/main/java/Board.java
@@ -0,0 +1,11 @@
+public class Board {
+    private int size;
+    private int mines;
+    private char mine = '*';
+    private char unknown = '-';
+    private char empty = ' ';
+    private char[][] board;
+
+
+
+}
