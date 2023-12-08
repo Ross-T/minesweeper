e88596ca16bb57f9024594dbf0aed945cd742c87
Ross-T <ross_turpin@yahoo.co.uk>
2023-12-08 10:44:55 +0000

    Added placeMines() method

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,15 +1,22 @@
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Random;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Board {
-    private int size;
-    private int mines;
+    private int size = 10;
+    private int mines = 10;
     private char mine = '*';
-    private char unknown = '-';
+    private char unrevealed = '□';
     private char empty = ' ';
     private char[][] board = new char[size][size];
+    private char[][] displayBoard = new char[size][size];
 
 
     void createBoard() {
@@ -19,5 +26,28 @@ public class Board {
             }
         }
     }
+
+    void createDisplayBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                displayBoard[i][j] = unrevealed;
+            }
+        }
+    }
+
+    void placeMines() {
+        Random rand = new Random();
+        int minesPlaced = 0;
+        while (minesPlaced < mines) {
+            int x = rand.nextInt(size);
+            int y = rand.nextInt(size);
+            if (board[x][y] != mine) {
+                board[x][y] = mine;
+                minesPlaced++;
+            }
+        }
+    }
+
+
 
 }
